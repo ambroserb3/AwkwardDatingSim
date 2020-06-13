f9f341d8817b0e14e8ef7aec5b8b7ddb550e8a2d
Stephan Brown <30557024+stephanrb3@users.noreply.github.com>
2020-06-13 17:21:26 -0400

    scripts
    question, topic, answer

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,54 @@
-var lnStickyNavigation;
+// Core dating logic goes here!
 
 $(document).ready(function()
 {	
-	applyHeader();
-	applyNavigation(); 
+	//function to load for ready
 });
 
+$.getJSON("dating.json", function(json) {
+	// console.log(json); // this will show the info it in firebug console
+	// console.log(json.Categories.Emotional.Questions)
+	let cat = chooseCategory(json)
+	chooseQuestion(json);
+	answerpool(json)
+});
+
+function chooseQuestion(json){
+	//need to feed category as input as well
+	let r = getRandom(0, json.Categories.Emotional.Questions.length)
+	// console.log(r)
+	// console.log(json.Categories.Emotional.Questions[r])
+	document.getElementById("question").innerHTML = json.Categories.Emotional.Questions[r]; 
+}
+
+function chooseCategory(json){
+	let r = getRandom(0, json.Categories.length)
+	// console.log(json.Categories.length)
+	// console.log(r)
+	let category = json.Categories[r]
+	// document.getElementById("cat").innerHTML = json.Categories[r]; 
+	return category
+}
+
+function answerpool(json){
+	let answers = []
+	for (let i = 0; i >=3; i++) {
+			// need to prevent dupes
+			console.log("hi")
+			let r = getRandom(0, json.Categories.Emotional.Answers.length)
+			console.log(r)
+			console.log(json.Categories.Emotional.Answers[r])
+			answers.push(json.Categories.Emotional.Answers[r])
+		}
+	console.log(answers)
+	document.getElementById("answer1").innerHTML = answers[0]; 
+	document.getElementById("answer2").innerHTML = answers[1]; 
+	document.getElementById("answer3").innerHTML = answers[2]; 
+}
+
+/**
+ * Returns a random number between min (inclusive) and max (exclusive)
+ */
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
