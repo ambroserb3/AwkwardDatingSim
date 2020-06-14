d84120f9bd9fa580806fe18a5c8ea466c81bf305
Stephan Brown <30557024+stephanrb3@users.noreply.github.com>
2020-06-14 15:03:50 -0400

    lobby changes

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => res.render('pages/index'))
-app.get('/lobby', function(req, res) {
+app.get('/game', function(req, res) {
   res.render('pages/rooms', { });
 });
 app.get('/char', function(req, res) {
@@ -25,17 +25,101 @@ app.get('/date', function(req, res) {
 });
 
 var usernames = {};
-var rooms = ['Lobby'];
+var rooms = ['game'];
+
+function Player(id, name, numbers, isPlaying){
+  this.id = id;
+  this.name = name;
+  this.numbers = numbers;
+  this.isPlaying = isPlaying;
+}
+
+function Game(playerA, playerB, numbers, operators, movingPlayerId) {
+  this.playerA = playerA;
+  this.playerB = playerB;
+  this.operators = operators;
+  this.movingPlayerId = movingPlayerId;
+}
+
+players = [];
+game = new Game();
+
+
+var randomNumber = Math.floor(Math.random() * (21 - 2 + 1)) + 2;
+var newNumber = 0;
+var info = ' ';
+
+function executeNextMove(movingPlayer, waitingPlayer) {
+  number = waitingPlayer.numbers[waitingPlayer.numbers.length-1];
+  operator = game.operators[game.operators.length-1];
+  newNumber = calcNewNumber(number, operator);
+  movingPlayer.numbers.push(newNumber);
+  movingPlayer.isPlaying = false;
+  waitingPlayer.isPlaying = true;
+
+  game.movingPlayerId = waitingPlayer.id;
+  info_waiting = '...waiting for ' + waitingPlayer.name + ' to a move!';
+  info_moving = 'Please make a move ' + waitingPlayer.name + '!'
+  
+  io.sockets.connected[movingPlayer.id].emit('info', info_waiting);
+
+  if(waitingPlayer.id != 'notAvailable') {
+      io.sockets.connected[waitingPlayer.id].emit('info', info_moving);
+  }
+  io.sockets.emit('game', game);
+}
+
+function calcNewNumber(number, operator){
+  newNumber = (number + operator) / 3;
+  return Math.round(newNumber);
+};
+
+function getCurrentPlayerName(game) {
+  return (game.movingPlayerId === game.playerA.id) ? game.playerA.name : game.playerB.name;
+}
+
+function togglePlayer() {
+   game.movingPlayerId === game.playerA.id ? game.playerA.id : game.playerB.id; 
+}
 
 io.sockets.on('connection', function(socket) {
     socket.on('adduser', function(username) {
         socket.username = username;
-        socket.room = 'Lobby';
+        socket.room = 'game';
+
+        players.push(socket);
+        if(players.length === 1){   
+            player = new Player(socket.id, 'Player A', [], true);
+            game.playerA = player;
+            game.playerB = new Player('notAvailable', 'Player B', [randomNumber], false);
+            game.movingPlayerId = game.playerA.id;
+            game.operators = [];
+            info = 'Hi Player A, please make a move.'
+            io.sockets.connected[socket.id].emit('info', info);
+            //emit game user only to our first player
+            io.sockets.connected[socket.id].emit('game', game);
+        } else if(players.length === 2) {
+            game.playerB.id = socket.id;
+            if(game.movingPlayerId === game.playerA.id) {
+                info = 'Hi Player B! ...waiting for Player A';
+            } else {
+                info = 'Hi Player B! Please make a move.';
+                game.movingPlayerId = game.playerB.id;
+            }
+          
+            io.sockets.connected[socket.id].emit('info', info);
+            io.sockets.emit('game', game);
+        } else {
+            info = "Sorry, there are already two guys playing...!"
+            io.sockets.connected[socket.id].emit('info', info);
+            io.sockets.emit('game', game);
+        };
         usernames[username] = username;
-        socket.join('Lobby');
-        socket.emit('updatechat', 'SERVER', 'you have connected to Lobby');
-        socket.broadcast.to('Lobby').emit('updatechat', 'SERVER', username + ' has connected to this room');
-        socket.emit('updaterooms', rooms, 'Lobby');
+        console.log(usernames)
+        socket.join('game');
+        socket.emit('updatechat', 'SERVER', 'you have connected to game');
+        socket.broadcast.to('game').emit('updatechat', 'SERVER', username + ' has connected to this room');
+        socket.emit('updaterooms', rooms, 'game');
         console.log("A USER HAS been added")
 
     });
@@ -61,11 +145,37 @@ io.sockets.on('connection', function(socket) {
         socket.emit('updaterooms', rooms, newroom);
     });
 
+    socket.on('next-move', function(data){ 
+        game.operators.push(parseInt(data));
+        if(game.movingPlayerId === game.playerA.id) {
+            executeNextMove(game.playerA, game.playerB);
+        } else {
+            executeNextMove(game.playerB, game.playerA);
+        }
+        if (newNumber === 1) {
+            //because movingPlayerId was just switched
+            if (game.playerB.id.match(game.movingPlayerId)){
+                info = "The winner is Player A";
+            } else {
+                info = "The winner is Player B";
+            }
+            io.sockets.emit('info', info);
+            //io.close();
+        }
+    });
+  
     socket.on('disconnect', function() {
+        //remove player from list
+        var index = players.indexOf(socket);
+        if (index != -1) {
+            players.splice(index, 1);
+        }
+        //TODO error handling for game
         delete usernames[socket.username];
         io.sockets.emit('updateusers', usernames);
         socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
         socket.leave(socket.room);
+        //io.close();
     });
  });
 
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -46,9 +46,91 @@ function answerpool(category){
 	document.getElementById("answer3").innerHTML = answers[2]; 
 }
 
-/**
- * Returns a random number between min (inclusive) and max (exclusive)
- */
 function getRandom(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
+
+
+///////////////////////////////////////////////////////////
+var socket = io();
+
+socket.on('game', function (data) {
+    render(data);
+});
+
+socket.on('info', function (data) {
+    renderInfo(data);
+});
+
+function render(data) {
+    var movingPlayer;
+    var waitingPlayer;
+
+    //get all operator buttons
+    var button = document.getElementsByTagName('button');
+    var buttonList = Array.prototype.slice.call(button);
+
+    //set styling for moving player
+    if (data.playerA.isPlaying) {
+        movingPlayer = document.getElementById('player-a');
+        waitingPlayer = document.getElementById('player-b');
+    } else {
+        movingPlayer = document.getElementById('player-b');
+        waitingPlayer = document.getElementById('player-a');
+    }
+    movingPlayer.className = 'active';
+    waitingPlayer.className = '';
+
+    if (data.movingPlayerId === socket.id) {
+        buttonList.forEach(function (button) {
+            button.disabled = false;
+        });
+    } else {
+        buttonList.forEach(function (button) {
+            button.disabled = true;
+        });
+    }
+
+    if (data.playerB.numbers && data.playerA.numbers && data.operators) {
+
+        var playerA = data.playerA.numbers.map(function (numberA, index) {
+            return (`<div><div>${numberA}</div></div>`)
+        }).join(' ');
+
+        var operator = data.operators.map(function (operator, index) {
+            return (`<div><div>${operator}</div></div>`)
+        }).join(' ');
+
+        var playerB = data.playerB.numbers.map(function (numberB, index) {
+            return (`<div><div>${numberB}</div></div>`)
+        }).join(' ');
+
+        var div_playerA = document.getElementById('container-a');
+        div_playerA.innerHTML = playerA;
+
+        var div_operators = document.getElementById('container-operator');
+        div_operators.innerHTML = operator;
+
+        var div_playerB = document.getElementById('container-b');
+        div_playerB.innerHTML = playerB;
+
+        var div_play = document.getElementById('playground');
+        div_play.scrollTop = div_play.scrollHeight;
+    }
+}
+
+function nextMove(operator) {
+    socket.emit('next-move', operator);
+}
+
+function renderInfo(data) {
+    var div_info = document.getElementById('info-box');
+    div_info.innerHTML = data;
+}
+
+function toggleAudio() {
+    var sound = document.getElementById("sound");
+    return sound.paused ? sound.play() : sound.pause();
 }
