821705ef38a059369c6270df07a73f23800bf75c
Stephan Brown <30557024+stephanrb3@users.noreply.github.com>
2020-06-14 11:15:38 -0400

    rooms.ejs
    lobby and rooms test

--- index.js
+++ index.js
@@ -15,15 +15,63 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => res.render('pages/index'))
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  let output = "hey"
-  socket.emit("Output", output)
-  socket.on("Input", async() => {
-      let output = "hey"
-      socket.emit("Output", output)
-  })
-});
+// io.on('connection', (socket) => {
+//   console.log('a user connected');
+//   let output = "hey"
+//   socket.emit("Output", output)
+//   socket.on("Input", async() => {
+//       let output = "hey"
+//       socket.emit("Output", output)
+//   })
+
+//   socket.on('disconnect', () => console.log('Client disconnected'));
+// });
+
+var usernames = {};
+
+var rooms = ['Lobby'];
+
+io.sockets.on('connection', function(socket) {
+    socket.on('adduser', function(username) {
+        socket.username = username;
+        socket.room = 'Lobby';
+        usernames[username] = username;
+        socket.join('Lobby');
+        socket.emit('updatechat', 'SERVER', 'you have connected to Lobby');
+        socket.broadcast.to('Lobby').emit('updatechat', 'SERVER', username + ' has connected to this room');
+        socket.emit('updaterooms', rooms, 'Lobby');
+    });
+
+    socket.on('create', function(room) {
+        rooms.push(room);
+        socket.emit('updaterooms', rooms, socket.room);
+    });
+
+    socket.on('sendchat', function(data) {
+        io.sockets["in"](socket.room).emit('updatechat', socket.username, data);
+    });
+
+    socket.on('switchRoom', function(newroom) {
+        var oldroom;
+        oldroom = socket.room;
+        socket.leave(socket.room);
+        socket.join(newroom);
+        socket.emit('updatechat', 'SERVER', 'you have connected to ' + newroom);
+        socket.broadcast.to(oldroom).emit('updatechat', 'SERVER', socket.username + ' has left this room');
+        socket.room = newroom;
+        socket.broadcast.to(newroom).emit('updatechat', 'SERVER', socket.username + ' has joined this room');
+        socket.emit('updaterooms', rooms, newroom);
+    });
+
+    socket.on('disconnect', function() {
+        delete usernames[socket.username];
+        io.sockets.emit('updateusers', usernames);
+        socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+        socket.leave(socket.room);
+    });
+ });
+
+
 server.listen(PORT, () => console.log("Server started!", `Listening on ${ PORT }`));
 ////////////////////////////////////////////////////////////////////////////////////////////
 
