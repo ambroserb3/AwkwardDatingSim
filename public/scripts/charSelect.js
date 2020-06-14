b75dd6fd98f7228b884d2cce4897f3f0fd470c2f
ile123ile <bbravencofounder@gmail.com>
2020-06-14 15:01:03 -0700

    made char select send the number over

--- public/scripts/charSelect.js
+++ public/scripts/charSelect.js
@@ -1,7 +1,8 @@
 var socket = io()
 
 $(function(){
-    $('.selectChar').click( function(choice) {
+    $('.selectChar').click( function(event) {
+    	let choice = $(this).data('char')
     	socket.emit('selectChar', choice)
 
     	window.location.href = '/date'
