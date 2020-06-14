8c28c5c61bd1b28f6a7a8c6917e082e7523f8750
Stephan Brown <30557024+stephanrb3@users.noreply.github.com>
2020-06-14 13:00:49 -0400

    fix jquery load

--- public/scripts/play.js
+++
@@ -1,9 +0,0 @@
-const socket = io();
-console.log("fuck")
-
-document.getElementById("play").addEventListener("click", playbutton);
-function playbutton() {
-    console.log("clicked")
-    // Join game
-    socket.emit('play');
-}
